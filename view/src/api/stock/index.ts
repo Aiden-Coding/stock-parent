9b7bc4a198af7ae0c6b97ff230d040e7c1df7c6b
dong <dwx_job@163.com>
2023-07-22 22:24:12 +0800

    init

--- view/src/api/stock/index.ts
+++ view/src/api/stock/index.ts
@@ -1,18 +1,18 @@
 import request from '@/config/axios'
 
 export const getMyGroupApi = (): Promise<IResponse> => {
-  return request.get({ url: '/group/grouplist' })
+  return request.get({ url: '/userGroup/grouplist' })
 }
 
 export const addGroupApi = (data: any): Promise<IResponse> => {
-  return request.post({ url: '/group/add', data })
+  return request.post({ url: '/userGroup/add', data })
 }
 
 export const deleteGroupApi = (data: any): Promise<IResponse> => {
   return request.delete({ url: '/stock/myGroup/delete', data })
 }
 export const groupStockListApi = (data: any): Promise<IResponse> => {
-  return request.post({ url: '/group/groupStock', data })
+  return request.post({ url: '/userGroup/groupStock', data })
 }
 
 export const addStockApi = (groupId: any): Promise<IResponse> => {
